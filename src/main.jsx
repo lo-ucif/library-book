270c242e1ec03f1e65f4a52fdefc9a1e99e308b8
Loucif Tameur Ahmed <tameur_ahmed.loucif@univ-constantine2.dz>
2025-12-23 18:51:30 +0100

    change07

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import "@fontsource/inter";
-
+import * as bootstrap from "bootstrap";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
