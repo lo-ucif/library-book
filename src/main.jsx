41490bd4efd75f80b2d34cd862bb35a0ba6d323f
Loucif Tameur Ahmed <tameur_ahmed.loucif@univ-constantine2.dz>
2025-12-23 18:54:48 +0100

    change08

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "animate.css";
 import App from "./App.jsx";
 import * as bootstrap from "bootstrap";
 createRoot(document.getElementById("root")).render(
